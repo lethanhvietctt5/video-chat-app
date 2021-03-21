5e1db13b3ede614fa59708f0ee7477f366de0e01
Le Thanh Viet <lethanhviet7c@gmail.com>
2021-03-22 01:53:12 +0700

    config some routes for test

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 const cors = require("cors");
 const connectDB = require("./config/db");
+const jwt = require("jsonwebtoken");
 
 app.use(express.static("public"));
 app.use(express.json({ extended: false }));
@@ -20,12 +21,23 @@ app.use((req, res, next) => {
 
 connectDB();
 
+app.get("/auth", function (req, res) {
+  const token = req.headers["x-auth-token"];
+  if (token) {
+    const decoded = jwt.verify(token, "shhhhh");
+    return res.status(200).json({ user: decoded.email });
+  }
+
+  res.status(403).json({ message: "Failed Auth" });
+});
+
 app.post("/login", function (req, res) {
   const { email, password } = req.body;
-  console.log(email === "lethanhviet7c@gmail.com" && password === "22102000");
-  if (email === "lethanhviet7c@gmail.com" && password === "22102000")
-    res.status(200).json({ email });
-  else res.status(403).json({ message: "Failed" });
+
+  if (email === "lethanhviet7c@gmail.com" && password === "22102000") {
+    const token = jwt.sign({ email: email, password: password }, "shhhhh");
+    res.status(200).json({ token, user: email });
+  } else res.status(403).json({ message: "Failed" });
 });
 
 const port = process.env.PORT || 5000;
